8a791f883cdb74eebd96c891803799de0828808b
driss292 <drisskaci@outlook.fr>
2022-08-25 12:42:45 +0200

    Tuple & Enum

---
+++ Contenu/I Les bases de TS/6. Tuple et Enum/script.ts
@@ -0,0 +1,20 @@
+// Tuple
+
+let tuple: [boolean, number];
+tuple = [false, 20];
+
+// Enum
+
+// const roles = {
+//     JAVASCRIPT: 1,
+//     CSS: 2,
+//     REACT: 3,
+// };
+// console.log(roles.JAVASCRIPT);
+
+enum Roles {
+    JAVASCRIPT = 1,
+    CSS,
+    REACT,
+}
+console.log(Roles.JAVASCRIPT);
